5cf82bc58f963e0b596f63135ed827e8f1c333bc
Qasim <69038179+Qasim345@users.noreply.github.com>
2022-01-16 14:57:19 +0430

    update main.js

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js").then(registeration => {
-    console.log("SW Registered!");
+   // console.log("SW Registered!");
   //  console.log(registeration);
   }).catch(error => {
     //console.log("SW Registered filed");
